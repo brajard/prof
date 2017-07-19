28d011b13e715d9a2ba3fdbef5681b92e6879549
brajard <julien.brajard@upmc.fr>
2017-07-19 15:53:54 +0200

    first import profile.py

---
+++ profile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 14:01:33 2017
+Class definition for profil reconstruction
+standard profile defines a class using results of script-acp.py
+@author: brajard
+"""
+
+class profile:
+    __init__(preproc,pca,depth,interp=True,lat=None):
+        pass
+        #check shape of pca.components, lat
